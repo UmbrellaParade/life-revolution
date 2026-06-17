fc78b7d4015f8ff8ca98114a1e3b331a599f8ff4
UmbrellaParade <bellbo13@users.noreply.github.com>
2026-06-17 23:05:13 +0900

    fix: update GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/saving/' : '/',
+  base: process.env.GITHUB_PAGES ? '/life-revolution/' : '/',
   plugins: [react()],
 })
